59bc1a9d4db9b4f7955874408677bf707522a48e
Todd Wolfson <todd@twolfson.com>
2012-08-22 22:31:23 -0700

    Moved to .min.css, adding a colors.less

--- grunt.js
+++ grunt.js
@@ -4,15 +4,19 @@ module.exports = function (grunt) {
     less: {
       all: {
         src: 'public/css/index.less',
-        dest: 'dist/css/index.css',
+        dest: 'dist/css/index.min.css',
         options: {
           compress: true
         }
+      },
+      unmin: {
+        src: 'public/css/index.less',
+        dest: 'dist/css/index.css'
       }
     },
     watch: {
       less: {
-        files: 'public/css/**.less',
+        files: 'public/css/**/*.less',
         tasks: 'less'
       }
     }
