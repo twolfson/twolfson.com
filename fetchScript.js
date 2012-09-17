435f33382dfe2e98d0ab4e4014b93e84f1755f8f
Todd Wolfson <todd@twolfson.com>
2012-09-16 19:58:39 -0700

    Scraping for MDN is in

--- fetchScript.js
+++ fetchScript.js
@@ -1,8 +1,35 @@
 var gitter = require('gitter'),
     Repo = gitter.repo;
-    // repo = new Repo('twolfson', 'File-Watcher');
 
-Repo('mythz', 'jquip', function (err, repo) {
-  console.log('watchers', repo.watchers);
-  console.log('forks', repo.forks);
+// Repo('twolfson', 'File-Watcher', function (err, repo) {
+// // Repo('mythz', 'jquip', function (err, repo) {
+//   console.log('watchers', repo.watchers);
+//   console.log('forks', repo.forks);
+// });
+
+// https://developer.mozilla.org/en-US/demos/detail/indexeddb-editor
+var http = require('https');
+http.get({'host': 'developer.mozilla.org', 'path': '/en-US/demos/detail/indexeddb-editor'}, function (res) {
+  var html = "";
+  res.on('data', function (chunk) {
+    html += chunk;
+  });
+  res.on('end', function () {
+    var viewArr = html.match(/(\d+) views/),
+        likeArr = html.match(/(\d+) likes/);
+
+    if (viewArr && viewArr.length >= 2) {
+      var views = viewArr[1];
+      if (views !== undefined) {
+        console.log('views', views);
+      }
+    }
+
+    if (likeArr && likeArr.length >= 2) {
+      var likes = likeArr[1];
+      if (likes !== undefined) {
+        console.log('likes', likes);
+      }
+    }
+  });
 });
