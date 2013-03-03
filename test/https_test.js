ae7ebe8ad6f66ffd17911338475b4f7e208d4741
Todd Wolfson <todd@twolfson.com>
2013-03-03 01:47:28 -0800

    Added in HTTPS test

---
+++ test/https_test.js
@@ -0,0 +1,27 @@
+require('./setup');
+describe('twolfson.com', function () {
+  before(function (done) {
+    var that = this,
+        httpsPage = config.url('/').replace('http:', 'https:');
+    request.get(httpsPage, function getHttpsPage (err, res, body) {
+      that.err = err;
+      that.res = res;
+      that.body = body;
+      done(err);
+    });
+  });
+
+ it('is responding', function () {
+    assert(!this.err);
+  });
+
+  it('is responding with valid status code', function () {
+    var statusCode = this.res.statusCode;
+    assert(statusCode >= 200, statusCode + ' is not above 200');
+    assert(statusCode < 300, statusCode + ' is not under 300');
+  });
+
+  it('is responding with content', function () {
+    assert(this.body);
+  });
+});
