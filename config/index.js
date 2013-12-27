5bfae282d3f47843229c6fb6f7e480434b93317d
Todd Wolfson <todd@twolfson.com>
2013-12-26 20:46:37 -0800

    Adding donation endpoints

--- config/index.js
+++ config/index.js
@@ -7,5 +7,12 @@ module.exports = {
   env: env,
   mail: require('./mail'),
   inProduction: inProduction,
-  inDevelopment: !inProduction
+  inDevelopment: !inProduction,
+  'support-me': {
+    gittip: 'twolfson',
+    flattr: 'twolfson',
+    paypal: 'todd@twolfson.com',
+    bitcoin: 'TODO',
+    dogecoin: 'DGJQbYtSH8jau967XKUR7cpZ7jJEe9SPSQ'
+  }
 };
