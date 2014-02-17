12a0fc577b1867ec900a8d9d61fa67f6bdce94ba
Todd Wolfson <todd@twolfson.com>
2014-02-17 00:12:27 -0800

    Deleting reading time calculation

--- articles/index.js
+++ articles/index.js
@@ -61,22 +61,6 @@ articles.forEach(function (article) {
 
 // TODO: Add hidden projects that can be linked to articles (e.g. doubleshot for bdd-and-the-future)
 
-// Calculate reading speed for each article
-// DEV: Wiki says 250 words per minute so this is very generous timing
-// DEV: Currently, this is 2 minutes for free will and 9 minutes for develop faster
-var READING_SPEED = 100; // words per minute
-articles.forEach(function (article) {
-  // TODO: Make this more efficient by iterating rather than splitting
-  var txtContent = unhtml(article.content),
-      words = txtContent.split(/\s+/g).filter(function (word) {
-        // Remove strings without alphanumeric characters (e.g. `+`)
-        return word.match(/[A-Za-z0-9]/);
-      }),
-      wordCount = words.length,
-      readingTime = Math.round(wordCount / READING_SPEED);
-  article.readingTime = Math.max(readingTime, 1);
-});
-
 // Construct objects to look up articles and projects by
 // TODO: This is not the correct place to map relationships as projects can never read articles now =(
 var articleObj = {};
