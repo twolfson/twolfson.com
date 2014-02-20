721037708a53974720012b513d44bf470c14fb93
Todd Wolfson <todd@twolfson.com>
2014-02-20 02:46:22 -0800

    Completed reproduction and opened PhantomJS issue

--- test/perceptual-tests/screenshot.js
+++ test/perceptual-tests/screenshot.js
@@ -1,6 +1,11 @@
 var page = require('webpage').create();
+page.onLoadFinished = function (status) {
+  console.log('status2', status);
+};
 page.open('http://google.com/', function (status) {
     console.log('status', status);
-    console.log('render output', page.render('github.png'));
-    phantom.exit();
+    // setTimeout(function () {
+      console.log('render output', page.render('a.png'));
+      phantom.exit();
+    // }, 100);
 });
