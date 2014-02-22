04b5b982bb09dae55d8ab5c1f27922ed8ca37de7
Todd Wolfson <todd@twolfson.com>
2014-02-21 23:26:40 -0800

    Adding arrays to remaining controllers

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -9,35 +9,41 @@ var fs = require('fs'),
 // Render homepage
 exports.index = function (config) {
   var articles = config.articles;
-  return function renderIndex (req, res) {
-    res.render('index', {
-      page: 'blog',
-      articles: articles,
-      activity: activityHtml,
-      title: 'Todd Wolfson - Blog',
-      seoKeywords: 'Todd Wolfson, twolfson, twolfsn, blog, javascript, web dev',
-      seoDescription: 'Blog about JavaScript and development tools'
-    });
-  };
+  return [
+    function renderIndex (req, res) {
+      res.render('index', {
+        page: 'blog',
+        articles: articles,
+        activity: activityHtml,
+        title: 'Todd Wolfson - Blog',
+        seoKeywords: 'Todd Wolfson, twolfson, twolfsn, blog, javascript, web dev',
+        seoDescription: 'Blog about JavaScript and development tools'
+      });
+    }
+  ];
 };
 
 // Render article
 exports.article = function (config) {
   var article = config.article;
-  return  function renderArticle (req, res) {
-    res.render('article', {
-      page: 'blog',
-      article: article,
-      title: article.title + ' - Todd Wolfson',
-      seoKeywords: article.keywords,
-      seoDescription: article.txtSummary
-    });
-  };
+  return [
+    function renderArticle (req, res) {
+      res.render('article', {
+        page: 'blog',
+        article: article,
+        title: article.title + ' - Todd Wolfson',
+        seoKeywords: article.keywords,
+        seoDescription: article.txtSummary
+      });
+    }
+  ];
 };
 
 // Render RSS feed
 exports.rss = function (config) {
-  return function renderRss (req, res) {
-    res.render('xml', {layout: false, articles: config.articles});
-  };
+  return [
+    function renderRss (req, res) {
+      res.render('xml', {layout: false, articles: config.articles});
+    }
+  ];
 };
