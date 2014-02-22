867adde30c10abe5aa7d3e6570c5888d6b03b57c
Todd Wolfson <todd@twolfson.com>
2014-02-22 15:02:38 -0800

    Removed dev .only

--- test/integration-tests/rss_test.js
+++ test/integration-tests/rss_test.js
@@ -3,7 +3,7 @@ var xml2js = require('xml2js');
 var httpUtils = require('../utils/http');
 var serverUtils = require('../utils/server');
 
-describe.only('A request to /index.xml', function () {
+describe('A request to /index.xml', function () {
   serverUtils.run();
   httpUtils.save(serverUtils.getUrl('/index.xml'));
   before(function detectErrors (done) {
