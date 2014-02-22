f339a07fce581caa820aeccb7c49610e72557924
Todd Wolfson <todd@twolfson.com>
2014-02-21 23:46:46 -0800

    Destroying setup and replacing with utils

--- test/integration-tests/utils/server.js
+++ test/integration-tests/utils/server.js
@@ -1,15 +1,22 @@
-// Load in modules
-var fs = require('fs');
-global.fs = fs;
-global.assert = require('assert');
-global.request = require('request');
+var Server = require('../');
 
-// Load in DOM specific modules
-global.jsdom = require('jsdom');
-var jqueryPath =  __dirname + '/test_files/jquery.js';
-global.jqueryPath = jqueryPath;
-global.jquerySrc = fs.readFileSync(jqueryPath, 'utf8');
+exports.getSettings = function () {
+  return {
+    port: 1337
+  };
+};
 
+exports.run = function () {
+  var server;
+  var settings = exports.getSettings();
+  before(function startServer () {
+    server = new Server();
+    server.listen(settings.port);
+  });
+  after(function stopServer (done) {
+    server.destroy(done);
+  });
+};
 // Set up config
 var config = {
   host: 'http://127.0.0.1:8080',
@@ -19,9 +26,6 @@ var config = {
     return this.host + path;
   },
   navigateToRaw: function (options, cb) {
-    // Collect arguments
-    var args = [].slice.call(arguments);
-
     // If the options are a string
     if (typeof options === 'string') {
       options = config.url(options);
