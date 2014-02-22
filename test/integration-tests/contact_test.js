adbf38b2d74f1b1ff84f9b94bb58cdcac2eeb157
Todd Wolfson <todd@twolfson.com>
2014-02-22 02:56:24 -0800

    Verified downed server test is working

--- test/integration-tests/contact_test.js
+++ test/integration-tests/contact_test.js
@@ -3,26 +3,7 @@ var smtp = require('smtp-protocol');
 var httpUtils = require('../utils/http');
 var serverUtils = require('../utils/server');
 
-describe('A request to the /contact form', function () {
-  serverUtils.run();
-  httpUtils.save(serverUtils.getUrl('/contact'));
-
-  it('has form elements', function () {
-    expect(this.body).to.contain('<input');
-  });
-});
-
-describe.only('A submission to /contact', function () {
-  serverUtils.run();
-  before(function startSmtp () {
-    this.smtpServer = smtp.createServer(function handleReq (req) {
-
-    });
-    this.smtpServer.listen(1338);
-  });
-  after(function stopSmtp (done) {
-    this.smtpServer.close(done);
-  });
+function makeContactRequest() {
   before(function (done) {
     httpUtils._save({
       url: serverUtils.getUrl('/contact'),
@@ -34,6 +15,29 @@ describe.only('A submission to /contact', function () {
       }
     }).call(this, done);
   });
+}
+
+describe('A request to the /contact form', function () {
+  serverUtils.run();
+  httpUtils.save(serverUtils.getUrl('/contact'));
+
+  it('has form elements', function () {
+    expect(this.body).to.contain('<input');
+  });
+});
+
+describe('A submission to /contact', function () {
+  serverUtils.run();
+  before(function startSmtp () {
+    this.smtpServer = smtp.createServer(function handleReq (req) {
+
+    });
+    this.smtpServer.listen(1338);
+  });
+  after(function stopSmtp (done) {
+    this.smtpServer.close(done);
+  });
+  makeContactRequest();
 
   it('does not have form elements', function () {
     expect(this.err).to.equal(null);
@@ -46,12 +50,13 @@ describe.only('A submission to /contact', function () {
   });
 });
 
-describe.skip('A failing submission to /contact', function () {
+describe.only('A failing submission to /contact', function () {
   serverUtils.run();
+  makeContactRequest();
 
-  it('receives a prompt to try again later', function () {
+  it('receives a prompt to try again', function () {
     expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(500);
-    expect(this.body).to.match('Try again laterrrrr');
+    expect(this.res.statusCode).to.equal(502);
+    expect(this.body).to.match(/Could you please try that again\?/i);
   });
 });
