95fb70a005d10de2d15b7d6de98c84884a5fc958
Todd Wolfson <todd@twolfson.com>
2014-02-22 18:22:55 -0800

    Initializing with server token

--- config/error-loggers.js
+++ config/error-loggers.js
@@ -1,5 +1,6 @@
 // Load in dependencies
 var rollbar = require('rollbar');
+var rollbarConfig = require('./secret').rollbar;
 
 // Define our error loggers
 exports.common = function (params) {
@@ -9,8 +10,7 @@ exports.common = function (params) {
 };
 
 exports.production = function (params) {
-  rollbar.init(params);
-  console.log('hai');
+  rollbar.init(rollbarConfig.serverToken, params);
   return function consoleRollbar (err, req) {
     console.error(arguments);
   };
