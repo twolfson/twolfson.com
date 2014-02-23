b63a4f4815bbf3bf28f11bf8e81cee5b055cd1c9
Todd Wolfson <todd@twolfson.com>
2014-02-22 19:04:29 -0800

    Moving to cache logger for test

--- test/integration-tests/error_test.js
+++ test/integration-tests/error_test.js
@@ -1,19 +1,24 @@
 var expect = require('chai').expect;
+var errorLoggers = require('../../config/error-loggers');
 var httpUtils = require('../utils/http');
 var serverUtils = require('../utils/server');
 
 describe.only('An error generating request', function () {
-  serverUtils.run();
+  var messages = [];
+  var logger = errorLoggers.cache({messages: messages});
+  serverUtils.run({
+    errorLogger: logger,
+    throwCaughtErrors: false
+  });
   httpUtils.save(serverUtils.getUrl('/errors/assertion'));
 
   it('sends a 500 page', function () {
-    console.log(this.err);
     expect(this.err).to.equal(null);
     expect(this.res.statusCode).to.equal(500);
     expect(this.body).to.contain('Todd Wolfson');
   });
 
-  it.skip('logs an error to `errorLogger`', function () {
-
+  it('logs an error to `errorLogger`', function () {
+    expect(messages).to.have.property('length', 1);
   });
 });
