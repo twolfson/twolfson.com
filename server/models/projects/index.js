1b537824891df4915a5d5a588c925601fc07419b
Todd Wolfson <todd@twolfson.com>
2014-02-23 06:04:09 -0800

    Adding in save functionality

--- server/models/projects/index.js
+++ server/models/projects/index.js
@@ -1,6 +1,8 @@
 // Load in the dependencies
 var fs = require('fs');
+var async = require('async');
 var Backbone = require('backbone');
+// TODO: Load these on initialize?
 var competitionsJson = require('./competitions');
 var CompetitionProject = require('./competition-project');
 var contributionsJson = require('./contributions');
@@ -8,10 +10,19 @@ var scriptsJson = require('./scripts');
 var ScriptProject = require('./script-project');
 
 // Define common collections
-var CompetitionCollection = Backbone.Collection.extend({
+var ProjectCollection = Backbone.Collection.extend({
+  initialize: function (models, options) {
+    Backbone.Collection.prototype.initialize.call(this, models);
+    this.filepath = options.filepath;
+  },
+  save: function (cb) {
+    fs.writeFile(this.filepath, this.toJSON(), cb);
+  }
+});
+var CompetitionCollection = ProjectCollection.extend({
   model: CompetitionProject
 });
-var ScriptCollection = Backbone.Collection.extend({
+var ScriptCollection = ProjectCollection.extend({
   model: ScriptProject
 });
 
@@ -27,29 +38,39 @@ var scriptModels = scriptsJson.map(function (scriptJson) {
 });
 
 // Generate and return collections for each project type
-module.exports = {
-  competitions: new CompetitionCollection(competitionModels, {
-    filepath: __dirname + '/competitions.json'
-  }),
-  contributions: new ScriptCollection(contributionModels, {
-    filepath: __dirname + '/contributions.json'
-  }),
-  scripts: new ScriptCollection(scriptModels, {
-    filepath: __dirname + '/scripts.json'
-  }),
-  update: function (cb) {
-    // TODO: Invoke async.parallel + update on all projects
-    process.nextTick(cb);
-  },
-  save: function (cb) {
-    // TODO: Save each toJSON format to disk
-    process.nextTick(cb);
-  },
-  toJSON: function () {
-    return {
-      competitions: this.competitions.toJSON(),
-      contributions: this.contributions.toJSON(),
-      scripts: this.scripts.toJSON()
-    };
-  }
+exports.competitions = new CompetitionCollection(competitionModels, {
+  filepath: __dirname + '/competitions.json'
+});
+exports.contributions = new ScriptCollection(contributionModels, {
+  filepath: __dirname + '/contributions.json'
+});
+exports.scripts = new ScriptCollection(scriptModels, {
+  filepath: __dirname + '/scripts.json'
+});
+
+exports.update  = function (cb) {
+  // TODO: Invoke async.parallel + update on all projects
+  process.nextTick(cb);
+};
+exports.save = function (done) {
+  // TODO: Save each toJSON format to disk
+  async.parallel([
+    function saveCompetitions (cb) {
+      exports.competitions.save(cb);
+    },
+    function saveContributions (cb) {
+      exports.contributions.save(cb);
+    },
+    function saveScripts (cb) {
+      exports.scripts.save(cb);
+    }
+  ], done);
+};
+
+exports.toJSON = function () {
+  return {
+    competitions: this.competitions.toJSON(),
+    contributions: this.contributions.toJSON(),
+    scripts: this.scripts.toJSON()
+  };
 };
