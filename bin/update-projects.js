7e52bb059bf9f94614c698157c55c6c51c12f621
Todd Wolfson <todd@twolfson.com>
2014-02-23 05:47:09 -0800

    Fixed up update projects script

--- bin/update-projects.js
+++ bin/update-projects.js
@@ -1,36 +1,42 @@
 // TODO: Get this working...
 var fs = require('fs');
 var projects = require('../server/models/projects');
-// updateStats(function handleStatsUpdate () {
-//   // Sort the scripts and contributions by stars then forks
-//   function sortRepos(a, b) {
-//     var aStars = a.stars;
-//     var bStars = b.stars;
-//     if (aStars < bStars) { return 1; }
-//     if (aStars > bStars) { return -1; }
+projects.update(function (err) {
+  // If there was an error, throw it
+  if (err) {
+    throw err;
+  }
 
-//     var aForks = a.forks;
-//     var bForks = b.forks;
-//     if (aForks < bForks) { return 1; }
-//     if (aForks > bForks) { return -1; }
+  // Otherwise, sort each collection (by stars then forks)
+  function sortProjects(a, b) {
+    var aStars = a.get('stars');
+    var bStars = b.get('stars');
+    if (aStars < bStars) { return 1; }
+    if (aStars > bStars) { return -1; }
 
-//     var aName = a.name;
-//     var bName = b.name;
-//     if (aName > bName) { return 1; }
-//     if (aName < bName) { return -1; }
-//     return 0;
-//   }
-//   scripts.sort(sortRepos);
-//   contributions.sort(sortRepos);
+    var aForks = a.get('forks');
+    var bForks = b.get('forks');
+    if (aForks < bForks) { return 1; }
+    if (aForks > bForks) { return -1; }
 
-//   // In a minute, save the updates to their respective JSON files
-//   setTimeout(function () {
-//     fs.writeFileSync(scriptsFile, JSON.stringify(scripts, null, 2), 'utf8');
-//     fs.writeFileSync(competitionsFile, JSON.stringify(competitions, null, 2), 'utf8');
-//     fs.writeFileSync(contributionsFile, JSON.stringify(contributions, null, 2), 'utf8');
+    var aName = a.get('name');
+    var bName = b.get('name');
+    if (aName > bName) { return 1; }
+    if (aName < bName) { return -1; }
+    return 0;
+  }
+  contributions.sortBy(sortProjects);
+  scripts.sortBy(sortProjects);
 
-//     // Exit the program
-//     console.log('Projects should be updated');
-//     process.exit();
-//   }, 2000);
-// });
+  // Save the projects to disk
+  projects.save(function (err) {
+    // If there was an error, throw it
+    if (err) {
+      throw err;
+    }
+
+    // Otherwise, exit the program
+    console.log('Projects should be updated');
+    process.exit();
+  });
+});
