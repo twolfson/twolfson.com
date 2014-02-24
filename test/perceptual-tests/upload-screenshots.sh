2c009d4db1a2549dad06c1e1dd4e4309b526373d
Todd Wolfson <todd@twolfson.com>
2014-02-23 22:10:10 -0800

    Getting result in JSON =D

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -3,5 +3,5 @@
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
 api_key="b3625162d3418ac51a9ee805b1840452"
 filepath="test/perceptual-tests/actual_screenshots/%2F.png"
-result="$(curl http://imgur.com/api/upload.xml -H "Expect: " -F "key=$api_key" -F "image=@$filepath" )"
+result="$(curl http://imgur.com/api/upload.json -H "Expect: " -F "key=$api_key" -F "image=@$filepath" )"
 echo "$result"
