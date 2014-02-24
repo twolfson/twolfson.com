fa201399212671ccd4f6681745f682338af8039c
Todd Wolfson <todd@twolfson.com>
2014-02-23 21:58:42 -0800

    Adding verbose call to debug latency

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -3,4 +3,4 @@
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
 api_key="b3625162d3418ac51a9ee805b1840452"
 filepath="test/perceptual-tests/actual_screenshots/%2F.png"
-curl http://imgur.com/api/upload.xml -H "Expect: " -F "key=$api_key" -F "image=@$filepath"
+curl -v http://imgur.com/api/upload.xml -H "Expect: " -F "key=$api_key" -F "image=@$filepath"
