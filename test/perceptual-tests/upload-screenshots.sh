15ad7ac5743bb5fcc84822bf16669f3d4452e7fe
Todd Wolfson <todd@twolfson.com>
2014-02-23 23:00:10 -0800

    Moving to wget since Travis doesn't link URLs

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -4,6 +4,8 @@
 cd test/perceptual-tests
 
 # Prepare location to collect delete commands
+output_dir="tmp/travis/$TRAVIS_BUILD_NUMBER"
+download_cmds=""
 delete_cmds=""
 
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
@@ -12,17 +14,21 @@ api_key="b3625162d3418ac51a9ee805b1840452"
 
 # for filepath in $(find {actual,diff}_screenshots -name '%2F.png'); do
 for filepath in $(find diff_screenshots -name '%2F.png'); do
-  result="$(curl http://imgur.com/api/upload.json -H "Expect: " -F "key=$api_key" -F "image=@$filepath" )"
+  # result="$(curl http://imgur.com/api/upload.json -H "Expect: " -F "key=$api_key" -F "image=@$filepath" )"
+  result='{"rsp":{"stat":"ok","image":{"image_hash":"dKZ0YK9","delete_hash":"r0MsZp11K9vawLf","original_image":"http:\/\/i.imgur.com\/dKZ0YK9.png","large_thumbnail":"http:\/\/i.imgur.com\/dKZ0YK9l.jpg","small_thumbnail":"http:\/\/i.imgur.com\/dKZ0YK9s.jpg","imgur_page":"http:\/\/imgur.com\/dKZ0YK9","delete_page":"http:\/\/imgur.com\/delete\/r0MsZp11K9vawLf"}}}'
   if test "$(echo "$result" | underscore extract 'rsp.stat')" != '"ok"'; then
     echo "There was a problem uploading \"$filepath\"" 1>&2
     echo "$result" 1>&2
   else
-    echo "Uploaded \"$filepath\" -> $(echo "$result" | underscore extract 'rsp.image.original_image')"
-    delete_cmds="$delete_cmds curl http://imgur.com/api/delete/$(echo "$result" | underscore extract 'rsp.image.delete_hash' --outfmt text).json;"
+    download_cmds="${download_cmds}wget -O \"$output_dir/$filepath\" $(echo "$result" | underscore extract 'rsp.image.original_image')\n"
+    delete_cmds="${delete_cmds}curl http://imgur.com/api/delete/$(echo "$result" | underscore extract 'rsp.image.delete_hash' --outfmt text).json;"
   fi
 done
 
 echo "All uploads complete!"
 echo ""
+echo "Download via:"
+echo "    mkdir -p $output_dir"
+echo "    $download_cmds"
 echo "Delete via:"
 echo "    $delete_cmds"
