b32e2ff5aea0938c1f1af6b03f7b3fbb85df1f48
Todd Wolfson <todd@twolfson.com>
2014-02-23 21:57:40 -0800

    Fixing up test

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
 api_key="b3625162d3418ac51a9ee805b1840452"
-filepath="test/perceptual-diffs/actual_screenshots/%2F.png"
+filepath="test/perceptual-tests/actual_screenshots/%2F.png"
 curl http://imgur.com/api/upload.xml -H "Expect: " -F "key=$api_key" -F "image=@$filepath"
