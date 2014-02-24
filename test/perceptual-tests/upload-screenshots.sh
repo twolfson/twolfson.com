384dbbbf91c681ad1f8b12ecdb6a5216d5c04305
Todd Wolfson <todd@twolfson.com>
2014-02-23 22:30:31 -0800

    Collecting deletion commands into one location

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -5,6 +5,8 @@ if ! which underscore &> /dev/null; then
   sudo npm install -g underscore-cli
 fi
 
+delete_cmds=""
+
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
 # Documentation: http://code.google.com/p/imgur-api/source/browse/wiki/ImageUploading.wiki?r=82
 api_key="b3625162d3418ac51a9ee805b1840452"
@@ -17,6 +19,8 @@ if test "$(echo "$result" | underscore extract 'rsp.stat')" != '"ok"'; then
 else
   echo "Uploaded \"$filepath\""
   echo "    Image URL: $(echo "$result" | underscore extract 'rsp.image.original_image')"
-  echo "    Image page: $(echo "$result" | underscore extract 'rsp.image.imgur_page')"
-  echo "    Delete via: $(echo "$result" | underscore extract 'rsp.image.delete_page')"
+  delete_cmds="$delete_cmds; curl http://imgur.com/api/delete/$(echo "$result" | underscore extract 'rsp.image.delete_hash').json"
 fi
+
+echo "Delete via:"
+echo "    $delete_cmds"
