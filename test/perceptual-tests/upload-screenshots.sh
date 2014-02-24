0f9f1c2aa48bd05e1c09796586766786cf3766a5
Todd Wolfson <todd@twolfson.com>
2014-02-23 22:13:07 -0800

    Found the documentation

--- test/perceptual-tests/upload-screenshots.sh
+++ test/perceptual-tests/upload-screenshots.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # curl from http://imgur.com/tools/imgurbash.sh via http://imgur.com/tools
+# Documentation: http://code.google.com/p/imgur-api/source/browse/wiki/ImageUploading.wiki?r=82
 api_key="b3625162d3418ac51a9ee805b1840452"
 filepath="test/perceptual-tests/actual_screenshots/%2F.png"
 result="$(curl http://imgur.com/api/upload.json -H "Expect: " -F "key=$api_key" -F "image=@$filepath" )"
