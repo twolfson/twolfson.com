5e6d00cdf396efedf0453d8bdf24bd96fe108e6f
Todd Wolfson <todd@twolfson.com>
2014-03-23 17:55:33 -0700

    Downgrading phantomjs to 1.9.2 since that should work

--- test/utils/install-phantomjs.sh
+++ test/utils/install-phantomjs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xvf phantomjs-1.9.7-linux-x86_64.tar.bz2
-sudo cp phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.2-linux-x86_64.tar.bz2
+tar xvf phantomjs-1.9.2-linux-x86_64.tar.bz2
+sudo cp phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/bin/
