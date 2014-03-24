37fdf9e1442fbacba7e6f4d62ab44bb80e537c98
Todd Wolfson <todd@twolfson.com>
2014-03-23 17:57:41 -0700

    Fixed URL for phantomjs@1.9.2

--- test/utils/install-phantomjs.sh
+++ test/utils/install-phantomjs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.2-linux-x86_64.tar.bz2
+wget http://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
 tar xvf phantomjs-1.9.2-linux-x86_64.tar.bz2
 sudo cp phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/bin/
