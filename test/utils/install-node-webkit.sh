f16069e84e2ddda2634154d797ebe447abbb4556
Todd Wolfson <todd@twolfson.com>
2014-11-21 22:50:27 -0800

    Adding node-webkit script

---
+++ test/utils/install-node-webkit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Load our arguments
+user="$1"
+if test "$user" = ""; then
+  echo "Expected \"user\" to be provided but it wasn't" 1>&2
+  echo "Usage: ./install-node-webkit.sh <user>" 1>&2
+  exit 1
+fi
+
+# Download and extract the file
+cd /tmp
+wget http://dl.node-webkit.org/v0.10.5/node-webkit-v0.10.5-linux-x64.tar.gz
+tar xvf node-webkit-v0.10.5-linux-x64.tar.gz
+
+# Copy over the files
+cp node-webkit-v0.10.5-linux-x64 /usr/local/lib/node-webkit/ -R
+ln -s /usr/local/lib/node-webkit/nw /usr/local/bin/nw
+
+# Adjust permissions
+chown "$user" -R /usr/local/lib/node-webkit/ -R
