90c2b052d19ee5a4a3e1a6324658c3281f69d344
Todd Wolfson <todd@twolfson.com>
2014-11-22 00:12:48 -0800

    Generating a lot of screenshot concurrently

--- test/perceptual-tests/node-webkit_scripts/screenshot.js
+++ test/perceptual-tests/node-webkit_scripts/screenshot.js
@@ -9,6 +9,7 @@ var assert = require('assert');
 var fs = require('fs');
 var gui = require('nw.gui');
 var path = require('path');
+var async = require('async');
 
 // Grab the arguments
 var url = gui.App.argv[0];
@@ -18,61 +19,64 @@ var imgDest = gui.App.argv[1];
 assert(url, 'No url was specified.');
 assert(imgDest, 'No img destination was specified.');
 
-// Navigate to a website in a new window
-// DEV: Otherwise, we lose our script after navigating
-var guiWidth = 800;
-var guiHeight = 600;
-// var win = gui.Window.open('http://google.com/', {
-var win = gui.Window.open(url, {
-  width: guiWidth,
-  height: guiHeight,
-  toolbar: false,
-  frame: false
-});
+async.times(100, function generateScreenshot (i, done) {
+  // Navigate to a website in a new window
+  // DEV: Otherwise, we lose our script after navigating
+  var guiWidth = 800;
+  var guiHeight = 600;
+  // var win = gui.Window.open('http://google.com/', {
+  var win = gui.Window.open(url, {
+    width: guiWidth,
+    height: guiHeight,
+    toolbar: false,
+    frame: false
+  });
 
-// When all the assets load (e.g. images, CSS, JS)
-win.on('loaded', function handleLoad () {
-  // Calculate how of the much window dimensions are padding
-  var viewportWidth = Math.max(
-    win.window.document.documentElement.clientWidth,
-    win.window.innerWidth || 0);
-  var viewportHeight = Math.max(
-    win.window.document.documentElement.clientHeight,
-    win.window.innerHeight || 0);
-  var paddingWidth = guiWidth - viewportWidth;
-  var paddingHeight = guiHeight - viewportHeight;
+  // When all the assets load (e.g. images, CSS, JS)
+  win.on('loaded', function handleLoad () {
+    // Calculate how of the much window dimensions are padding
+    var viewportWidth = Math.max(
+      win.window.document.documentElement.clientWidth,
+      win.window.innerWidth || 0);
+    var viewportHeight = Math.max(
+      win.window.document.documentElement.clientHeight,
+      win.window.innerHeight || 0);
+    var paddingWidth = guiWidth - viewportWidth;
+    var paddingHeight = guiHeight - viewportHeight;
 
-  // Resize to full content height/width
-  win.resizeTo(
-    win.window.document.body.scrollWidth + paddingWidth,
-    win.window.document.body.scrollHeight + paddingHeight);
+    // Resize to full content height/width
+    win.resizeTo(
+      win.window.document.body.scrollWidth + paddingWidth,
+      win.window.document.body.scrollHeight + paddingHeight);
 
-  // Wait for resize to take effect
-  // TODO: Place me on an async loop `async.until`
-  setTimeout(function waitForResize () {
-    // Wait for a bit longer
-    setTimeout(function waitForCanvasesToLoad () {
-      // Remove all canvas elements
-      var $canvases = win.window.document.getElementsByTagName('canvas');
-      [].forEach.call($canvases, function ($canvas) {
-        $canvas.parentNode.removeChild($canvas);
-      });
+    // Wait for resize to take effect
+    // TODO: Place me on an async loop `async.until`
+    setTimeout(function waitForResize () {
+      // Wait for a bit longer
+      setTimeout(function waitForCanvasesToLoad () {
+        // Remove all canvas elements
+        var $canvases = win.window.document.getElementsByTagName('canvas');
+        [].forEach.call($canvases, function ($canvas) {
+          $canvas.parentNode.removeChild($canvas);
+        });
 
-      // Render and exit
-      win.capturePage(function handleScreenshot (buff) {
-        // If there is no existing directory create one
-        try {
-          fs.mkdirSync(path.dirname(imgDest));
-        } catch (err) {
-          if (err.code !== 'EEXIST') {
-            throw err;
+        // Render and exit
+        win.capturePage(function handleScreenshot (buff) {
+          // If there is no existing directory create one
+          try {
+            fs.mkdirSync(path.dirname(imgDest));
+          } catch (err) {
+            if (err.code !== 'EEXIST') {
+              return done(err);
+            }
           }
-        }
 
-        // Write our our image and leave
-        fs.writeFileSync(imgDest, buff);
-        process.exit();
-      }, {format: 'png', datatype: 'buffer'});
-    }, 1000);
-  }, 100);
+          // Write our our image and leave
+          fs.writeFile(imgDest + i + '.png', buff, done);
+        }, {format: 'png', datatype: 'buffer'});
+      }, 1000);
+    }, 100);
+  });
+}, function allDone () {
+  process.exit();
 });
