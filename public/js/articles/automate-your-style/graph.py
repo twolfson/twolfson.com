a173d907b2272785d13b236493b7602b9a8c3552
Todd Wolfson <todd@twolfson.com>
2015-02-06 05:07:08 -0600

    Formalizing our code

--- public/js/articles/automate-your-style/graph.py
+++ public/js/articles/automate-your-style/graph.py
@@ -1,42 +1,67 @@
+# Load in our dependencies
 # Forking from http://matplotlib.org/xkcd/examples/showcase/xkcd.html
 from matplotlib import pyplot as plt
 import numpy as np
 
-plt.xkcd()
+"""
+ Comments on PRs about style
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-plt.xticks([])
-plt.yticks([])
-ax.set_ylim([-30, 10])
+20 | --------\
+   |          |
+   |          |
+   |          |
+   |          |
+ 1 |          \--\
+ 0 |              -------
+   -----------------------
+            |
+   Introduction of `jscs`
 
-data = np.ones(100)
-data[70:] -= np.arange(30)
+          Time
+"""
 
-plt.annotate(
-    'THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED',
-    xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
 
-plt.plot(data)
+def main():
+    """Generate and save an image as per the docstring above"""
+    # Define our style as
+    plt.xkcd()
 
-plt.xlabel('time')
-plt.ylabel('my overall health')
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    plt.xticks([])
+    plt.yticks([])
+    ax.set_ylim([-30, 10])
 
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1)
-# ax.bar([-0.125, 1.0-0.125], [0, 100], 0.25)
-# ax.spines['right'].set_color('none')
-# ax.spines['top'].set_color('none')
-# ax.xaxis.set_ticks_position('bottom')
-# ax.set_xticks([0, 1])
-# ax.set_xlim([-0.5, 1.5])
-# ax.set_ylim([0, 110])
-# ax.set_xticklabels(['CONFIRMED BY\nEXPERIMENT', 'REFUTED BY\nEXPERIMENT'])
-# plt.yticks([])
+    data = np.ones(100)
+    data[70:] -= np.arange(30)
 
-plt.title("CLAIMS OF SUPERNATURAL POWERS")
+    plt.annotate(
+        'THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED',
+        xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
 
-# plt.show()
-plt.savefig('hi.png')
+    plt.plot(data)
+
+    plt.xlabel('time')
+    plt.ylabel('my overall health')
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # ax.bar([-0.125, 1.0-0.125], [0, 100], 0.25)
+    # ax.spines['right'].set_color('none')
+    # ax.spines['top'].set_color('none')
+    # ax.xaxis.set_ticks_position('bottom')
+    # ax.set_xticks([0, 1])
+    # ax.set_xlim([-0.5, 1.5])
+    # ax.set_ylim([0, 110])
+    # ax.set_xticklabels(['CONFIRMED BY\nEXPERIMENT', 'REFUTED BY\nEXPERIMENT'])
+    # plt.yticks([])
+
+    plt.title("CLAIMS OF SUPERNATURAL POWERS")
+
+    # plt.show()
+    plt.savefig('hi.png')
+
+if __name__ == '__main__':
+    main()
