717caf4d704372d52ce109f84126337218efe0b7
Todd Wolfson <todd@twolfson.com>
2015-03-08 19:56:58 -0500

    Surprised at how <head> stayed accurate. We need to do node based comparison

--- tmp.js
+++ tmp.js
@@ -4,5 +4,12 @@ var request = require('request');
 
 // Make a request, normalize our HTML via Cheerio
 request('http://localhost:8080/', function (err, res, body) {
-  console.log(cheerio(body).html());
+  // If there is an error, throw it
+  if (err) {
+    throw err;
+  }
+
+  // Otherwise, coerce our data
+  var $ = cheerio.load(body);
+  console.log($('head').html());
 });
