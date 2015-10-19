3175215cf1d754127f6293b57d6575206112e5b6
Todd Wolfson <todd@twolfson.com>
2015-10-19 03:55:15 -0500

    Moved URL test into repo for permanence

--- test/integration-tests/legacy-urls_test.js
+++ test/integration-tests/legacy-urls_test.js
@@ -1,6 +1,7 @@
 // Load in our dependencies
 var async = require('async');
 var request = require('request');
+var serverUtils = require('../utils/server');
 
 // Define all URLs we expected
 var urlInfos = [
@@ -67,47 +68,56 @@ var urlInfos = [
 ];
 
 // For each of our URLs, verify it's a valid target
-async.map(urlInfos, function requestUrl (urlInfo, cb) {
-  var urlPathname = typeof urlInfo === 'object' ? urlInfo.src : urlInfo;
-  var url = 'http://localhost:8080' + urlPathname;
-  request({
-    url: url,
-    // TODO: When we finish this, we might need redirect: true
-    followRedirect: false
-  }, function handleResponse (err, res, body) {
-    // Callback with the error and response
-    return cb(err, res);
+// DEV: This was initially written as a one-off script -- hence the poor structure and bundling
+describe('When requesting cach of our legacy URLs', function () {
+  serverUtils.run();
+  before(function requestUrls (done) {
+    var that = this;
+    async.map(urlInfos, function requestUrl (urlInfo, cb) {
+      var urlPathname = typeof urlInfo === 'object' ? urlInfo.src : urlInfo;
+      var url = serverUtils.getUrl(urlPathname);
+      request({
+        url: url,
+        // TODO: When we finish this, we might need redirect: true
+        followRedirect: false
+      }, function handleResponse (err, res, body) {
+        // Callback with the error and response
+        return cb(err, res);
+      });
+    }, function saveResponses (err, resArr) {
+      that.resArr = resArr;
+      done(err);
+    });
   });
-}, function handleResponses (err, resArr) {
-  // If there was an error, throw it
-  if (err) {
-    throw err;
-  }
+  after(function cleanup () {
+    delete this.resArr;
+  });
 
-  // For each of our responses
-  resArr.forEach(function processRes (res, i) {
-    // If the info is a string, verify it was a direct hit
-    var urlInfo = urlInfos[i];
-    var urlPathname;
-    if (typeof urlInfo === 'string') {
-      urlPathname = urlInfo;
-      if (res.statusCode !== 200) {
-        console.error('Expected 200 status code for "' + urlPathname + '" but received "' + res.statusCode + '"');
-      }
-    // Otherwise, verify it was a 301 to the expected location
-    } else {
-      urlPathname = urlInfo.src;
-      if (res.statusCode !== 301) {
-        console.error('Expected 301 status code for "' + urlPathname + '" but received "' + res.statusCode + '"');
-        return;
-      }
+  it('renders/redirects to the expected location', function () {
+    // For each of our responses
+    this.resArr.forEach(function processRes (res, i) {
+      // If the info is a string, verify it was a direct hit
+      var urlInfo = urlInfos[i];
+      var urlPathname;
+      if (typeof urlInfo === 'string') {
+        urlPathname = urlInfo;
+        if (res.statusCode !== 200) {
+          throw new Error('Expected 200 status code for "' + urlPathname + '" but received "' + res.statusCode + '"');
+        }
+      // Otherwise, verify it was a 301 to the expected location
+      } else {
+        urlPathname = urlInfo.src;
+        if (res.statusCode !== 301) {
+          throw new Error('Expected 301 status code for "' + urlPathname + '" but received "' + res.statusCode + '"');
+        }
 
-      var actualLocation = res.headers.location;
-      if (actualLocation !== urlInfo.target) {
-        console.error('Expected "' + urlPathname + '" to redirect to "' + urlInfo.target + '" ' +
-          'but it redirected to "' + actualLocation + '"');
-        return;
+        var actualLocation = res.headers.location;
+        if (actualLocation !== urlInfo.target) {
+          throw new Error('Expected "' + urlPathname + '" to redirect to "' + urlInfo.target + '" ' +
+            'but it redirected to "' + actualLocation + '"');
+        }
       }
-    }
+    });
   });
 });
+
