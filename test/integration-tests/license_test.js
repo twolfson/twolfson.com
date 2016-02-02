c76bd408132c5527c62d1a6c502ae9d385cc8302
Todd Wolfson <todd@twolfson.com>
2016-02-01 18:26:34 -0600

    Added smoke test for license page

---
+++ test/integration-tests/license_test.js
@@ -0,0 +1,19 @@
+// Load in our dependencies
+var expect = require('chai').expect;
+var httpUtils = require('../utils/http');
+var serverUtils = require('../utils/server');
+
+// Define our tests
+describe('A request to /license', function () {
+  serverUtils.run();
+  httpUtils.save(serverUtils.getUrl('/license'));
+
+  it('has no errors', function () {
+    expect(this.err).to.equal(null);
+    expect(this.res.statusCode).to.equal(200);
+  });
+
+  it('renders our license', function () {
+    expect(this.body).to.contain('Permission is hereby granted');
+  });
+});
