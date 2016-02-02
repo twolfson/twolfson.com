f38aa491a9cef6046a1d5ff2541822faba2a9386
Todd Wolfson <todd@twolfson.com>
2016-02-01 18:27:44 -0600

    Added smoke test for /support-me and caught error

---
+++ test/integration-tests/support-me_test.js
@@ -0,0 +1,21 @@
+// Load in our dependencies
+var expect = require('chai').expect;
+var httpUtils = require('../utils/http');
+var serverUtils = require('../utils/server');
+
+// Define our tests
+describe('A request to /support-me', function () {
+  serverUtils.run();
+  httpUtils.save(serverUtils.getUrl('/support-me'));
+
+  it('has no errors', function () {
+    expect(this.err).to.equal(null);
+    expect(this.res.statusCode).to.equal(200);
+  });
+
+  it('renders our links to our support pages', function () {
+    expect(this.body).to.contain('Gratipay');
+    expect(this.body).to.contain('PayPal');
+    expect(this.body).to.contain('Bitcoin');
+  });
+});
