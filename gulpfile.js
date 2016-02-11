24f65f417f6585c3b690c9218347aad97036896f
Todd Wolfson <todd@twolfson.com>
2016-02-11 01:04:53 -0600

    Added configuration support for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ var rimraf = require('rimraf');
 
 // TODO: Make sure that all `/public/*` URLs work (e.g. `960.gridder`)
 
+// Set up our configuration
+var config = {
+  allowFailures: false,
+  minifyAssets: true
+};
+
 // Define our tasks
 gulp.task('build-clean', function clean (done) {
   // Remove all compiled files in `dist/`
@@ -16,12 +22,25 @@ gulp.task('build-clean', function clean (done) {
 gulp.task('build-css', function buildCss () {
   // Generate a stream that compiles SCSS to CSS
   // DEV: We return the pipe'd stream so gulp knows when we exit
-  return gulp.src('public/css/index.scss')
+  var cssStream = gulp.src('public/css/index.scss')
     .pipe(gulpSass({
       style: 'nested'
-    }))
-    .pipe(gulpCsso())
-    .pipe(gulpSizereport({gzip: true}))
+    }));
+
+  // If we are allowing failures, then log them
+  if (config.allowFailures) {
+    cssStream.on('error', console.error);
+  }
+
+  // If we are minifying assets, then minify them
+  if (config.minifyAssets) {
+    cssStream = cssStream
+      .pipe(gulpCsso())
+      .pipe(gulpSizereport({gzip: true}));
+  }
+
+  // Output our CSS
+  return cssStream
     .pipe(gulp.dest('dist/css'));
 });
 
