ea1d4bbdce811009627f2649ea51bcdd0175d069
Todd Wolfson <todd@twolfson.com>
2016-02-12 10:56:45 -0600

    Locked down URLs more

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -15,5 +15,7 @@ curl 'https://rawgit.com/ccampbell/gator/1.2.2/plugins/gator-legacy.js' > 'publi
 curl 'http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js' > 'test/test_files/jquery.js'
 
 # Download development dependencies
-curl 'https://rawgit.com/peol/960gridder/master/releases/1.3.1/960.gridder.src.js' > 'public/js/960gridder/960.gridder.js'
-curl 'http://peol.github.io/960gridder/releases/1.3.1/jquery.js' > 'public/js/960gridder/jquery.js'
+curl 'https://rawgit.com/peol/960gridder/677b61a7df2e6f83b6b4437fecb027fb94359f26/releases/1.3.1/960.gridder.src.js' \
+  > 'public/js/960gridder/960.gridder.js'
+curl 'https://rawgit.com/peol/960gridder/677b61a7df2e6f83b6b4437fecb027fb94359f26/releases/1.3.1/jquery.js' \
+  > 'public/js/960gridder/jquery.js'
