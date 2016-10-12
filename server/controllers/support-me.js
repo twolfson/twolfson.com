1bd784a884aa3afa46df3995ec8f71fd90ff499f
Todd Wolfson <todd@twolfson.com>
2016-10-12 05:08:03 -0700

    Updated notes

--- server/controllers/support-me.js
+++ server/controllers/support-me.js
@@ -5,26 +5,27 @@
 var SUPPORT_URLS = {
   paypal: 'https://www.paypal.me/twolfson'
 
-  // Unused options:
-  // Bitcoin - Exploring via Coinbase
-  //   https://developers.coinbase.com/docs/wallet/tutorials/donations
-
   // TODO: After exploring which is plausible
   //   Explore which is recurring/1 time and international support
   //   We prob don't want to overwhelm users with options
 
-  // Google Wallet - UNEXPLORED
-  // Amazon Payments - UNEXPLORED
-  // Square cash - UNEXPLORED
+  // Unused options:
+  // Coinbase - Should be good to use
+  //   https://developers.coinbase.com/docs/wallet/tutorials/donations
   // Stripe - Looks plausible, no server logic too which is great
   //   https://stripe.com/checkout#should-i
   // Patreon - Not designed for open source (e.g. no technology section, no GitHub links)
   //   but they have international support and recurring so it would be dope
   //   https://www.patreon.com/twolfson
 
+
+  // Google Wallet - UNEXPLORED
+  // Amazon Payments - UNEXPLORED
+  // Square cash - UNEXPLORED
+
   // Gratipay - NO (ditched due to poor UI and never getting approved)
   // Flattr - NO (ditched due to complexity)
-  // Dogecoin, Litecoin, etc - NO (don't want to manage own wallet)
+  // Bitcoin, Dogecoin, etc standalone - NO (don't want to manage own wallet)
   // Open collective - Not for single maintainers
 };
 
