b25ae7951c7ad021101550951ece697a42c3dab8
Todd Wolfson <todd@twolfson.com>
2016-11-01 00:56:32 -0700

    Added article test

---
+++ test/integration-tests/article_test.js
@@ -0,0 +1,25 @@
+// Load in our dependencies
+var expect = require('chai').expect;
+var httpUtils = require('../utils/http');
+var serverUtils = require('../utils/server');
+
+// Define our tests
+describe('A request to an article', function () {
+  serverUtils.run();
+  httpUtils.save(serverUtils.getUrl('/2012-02-21-blog-launch'));
+
+  it('has no errors', function () {
+    expect(this.err).to.equal(null);
+    expect(this.res.statusCode).to.equal(200);
+  });
+
+  it('has expected title', function () {
+    expect(this.$('title')).to.equal('Blog Launch - Todd Wolfson');
+  });
+
+  it('has article content', function () {
+    expect(this.body).to.contain('February 21, 2012');
+    expect(this.body).to.contain('Blog Launch!');
+    expect(this.body).to.contain('This is my first blog post for twolfson.com!');
+  });
+});
