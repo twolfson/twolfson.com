857abc8b9eb01e4164c22471cdd2ac37e1ab1c7d
Todd Wolfson <todd@twolfson.com>
2016-11-02 01:04:03 -0700

    Completed research

--- server/controllers/support-me.js
+++ server/controllers/support-me.js
@@ -18,24 +18,24 @@ var SUPPORT_URLS = {
   //   We prob don't want to overwhelm users with options
 
   // Unused options:
+  // Amazon Payments - Seems usable but not too practical (e.g. no variable donation)
+  //   https://payments.amazon.com/developer/documentation/express/201747030
   // Coinbase - Should be good to use (USD, BTC, ETH)
   //   Supports one time and recurring
   //     https://www.coinbase.com/merchant_tools
   //     https://developers.coinbase.com/docs/wallet/tutorials/donations
-  // Stripe - Looks plausible, no server logic too which is great
-  //   https://stripe.com/checkout#should-i
-  //   Supports recurring and BTC (but only with USA users? -- am I the user or is the customer the user?)
-  //   Supports most countries via conversion
-  //     https://support.stripe.com/questions/which-currencies-does-stripe-support
+  // Open collective - Can make a super collective for me as discussed over email
+  //   Backed by Stripe for donations, kind of pointless for our current wants
   // Patreon - Not designed for open source (e.g. no technology section, no GitHub links)
   //   but they have international support and recurring so it would be dope
   //   https://www.patreon.com/twolfson
   //   International support via: Visa, MasterCard, America Express, JCB, Discover, Diners Club and PayPal
   //     https://patreon.zendesk.com/hc/en-us/articles/204606275-Do-you-support-international-payments-Can-I-donate-in-any-currency-
-  // Open collective - Can make a super collective for me as discussed over email
-  //   Backed by Stripe for donations, kind of pointless for our current wants
-
-  // Amazon Payments - UNEXPLORED
+  // Stripe - Looks plausible, no server logic too which is great
+  //   https://stripe.com/checkout#should-i
+  //   Supports recurring and BTC (but only with USA users? -- am I the user or is the customer the user?)
+  //   Supports most countries via conversion
+  //     https://support.stripe.com/questions/which-currencies-does-stripe-support
 
   // Google Wallet - NO (limited to USA)
   //   https://www.google.com/wallet/
@@ -43,6 +43,7 @@ var SUPPORT_URLS = {
   // Flattr - NO (ditched due to complexity)
   // Venmo - NO (limited to USA)
   //   https://venmo.com/about/product/
+  //   https://help.venmo.com/hc/en-us/articles/226001687-General-Business-Partnership-Inquiries
   // Square Cash - NO (limited international support)
   //   https://squareup.com/help/us/en/article/4956-international-availability
   // Bitcoin, Dogecoin, etc standalone - NO (don't want to manage own wallet)
